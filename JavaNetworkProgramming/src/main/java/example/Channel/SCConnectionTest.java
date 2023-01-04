95a4ed314294abd6dd8ec316d2f759a439d466ab
ryeojin <sooon9254@naver.com>
2023-01-04 10:36:10 +0900

    SocketChannel, ServerSocketChannel 학습. 비동기 연결과 연결 상태 확인

---
+++ JavaNetworkProgramming/src/main/java/example/Channel/SCConnectionTest.java
@@ -0,0 +1,26 @@
+package example.Channel;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+
+public class SCConnectionTest {
+
+    private static int port = 8080;
+
+    public static void main(String[] args) throws Exception{
+        InetAddress ia = InetAddress.getLocalHost();
+        InetSocketAddress isa = new InetSocketAddress(ia, port);
+
+        SocketChannel sc = SocketChannel.open();
+        sc.configureBlocking(false);
+        System.out.println("Is ConnectionPending 1 : " + sc.isConnectionPending());
+        sc.connect(isa);
+        System.out.println("Is ConnectionPending 2 : " + sc.isConnectionPending());
+        sc.finishConnect();
+        System.out.println("Is ConnectionPending 3 : " + sc.isConnectionPending());
+
+        System.out.println("Is Connected : " + sc.isConnected());
+        System.out.println("Is Blocking Mode : " + sc.isBlocking());
+    }
+}
